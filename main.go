9d9d4f51c7f61721233dec0cc3b09a214f200b6d
Myavuz34 <myavuz53@gmail.com>
2021-02-02 16:35:07 +0300

    added os info

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println("www.myavuz.net")
+
+	uName := os.Getenv("USERNAME")
+	uDomain := os.Getenv("USERDOMAIN")
+	projessorArchiterture := os.Getenv("PROCESSOR_ARCHITECTURE")
+	// projessorIdentifier := os.Getenv("PROCESSOR_IDENTIFIER")
+	// processorLeve := os.Getenv("PROCESSOR_LEVEL")
+	goRoot := os.Getenv("GOROOT")
+	goPath := os.Getenv("GOPATH")
+	homePath := os.Getenv("HOMEPATH")
+
+	fmt.Println("Username :" + uName)
+	fmt.Println("Domain : " + uDomain)
+	fmt.Println("İşlemci Mimarisi : " + projessorArchiterture)
+	fmt.Println("HOMEPATH : " + homePath)
+	fmt.Println("GOPATH : " + goPath)
+	fmt.Println("GOROOT :" + goRoot)
 }
